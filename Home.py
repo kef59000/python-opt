b785576e3cb2a9084e68d99d6340d822e9000d88
florian.kellner <florian.kellner@ams-osram.com>
2022-10-29 22:53:26 +0200

    Sissi

--- Home.py
+++ Home.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC - Flo')
+st.title('Uber pickups in NYC - Flo + Sissi')
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
